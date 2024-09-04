0aec13edebe88df0f3524673919fc41cf7a31ddb
m.malyshonok <masha.malyshonok@gmail.com>
2024-09-04 11:33:51 +0500

    Initial commit

---
+++ src/test/java/ru/netology/ChangeDateTest.java
@@ -0,0 +1,61 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class ChangeDateTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting(){
+//        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 5;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='city'] input").setValue(DataGenerator.generateCity("ru"));
+        form.$("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id='date'] input").setValue(firstMeetingDate);
+        form.$("[data-test-id='name'] input").setValue(DataGenerator.generateName("ru"));
+        form.$("[data-test-id='phone'] input").setValue(DataGenerator.generatePhone("ru"));
+        form.$("[data-test-id='agreement'] .checkbox__box").click();
+        form.$(".button").click();
+        $("[data-test-id='success-notification'] .notification__title")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Успешно"));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+        form.$("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id='date'] input").setValue(secondMeetingDate);
+        form.$(".button").click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,66 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    //public static String generateCity(String locale) {
+        //String[] city = {"Сочи", "Москва", "Архангельск", "Санкт-Петербург", "Барнаул", "Якутск",
+                //"Липецк", "Новосибирск", "Великий Новгород", "Владивосток",
+                //"Киров", "Екатеринбург", "Волгоград", "Коломна", "Воронеж", "Пермь", "Ярославль",
+                //"Грозный", "Пенза", "Самара", "Рязань", "Зеленоград",
+                //"Йошкар-Ола", "Казань", "Калининград", "Орёл",
+                //"Уфа", "Тула", "Кострома", "Тамбов", "Тверь", "Курган"};
+
+        //return city[new Random().nextInt(city.length)];
+    //
+    //}
+
+    public static String generateCity(String local){
+        Faker faker = new Faker(new Locale(local));
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        String name = new Faker(new Locale(locale)).name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        String phone = new Faker(new Locale(locale)).phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+
+            String city = generateCity(locale);
+            String name = generateName(locale);
+            String phone = generatePhone(locale);
+            UserInfo user = new UserInfo(city, name, phone);
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
